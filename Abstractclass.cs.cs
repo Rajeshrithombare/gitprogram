aba2c37c4e548ab842af96ea876982b6c4815bf1
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-03-28 17:39:38 +0530

    myfirst commit

---
+++ Abstractclass.cs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class Account
+    {
+        public abstract void Deposite(int amt);
+
+        public void ShowData()
+        {
+            Console.WriteLine("balence");
+        }
+    }
+    class Saving:Account
+    {
+        public override void Deposite(int amt)
+        {
+            Console.WriteLine("Deposite");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account act = new Saving();
+            act.ShowData();
+            act.Deposite(2);
+            Console.ReadKey();
+        }
+    }
+}
