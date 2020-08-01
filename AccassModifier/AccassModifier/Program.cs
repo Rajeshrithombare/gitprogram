a4ef36cd1df150a193e586b9336e9457333b22fd
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-01 11:24:14 +0530

    Accass Modifier

---
+++ AccassModifier/AccassModifier/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AccassModifier
+{
+    class BaseClass
+    {
+        protected int Roll_N;
+
+    }
+    class ChildClass : BaseClass
+    {
+        public void GetData(int rno)
+        {
+            Roll_N = rno;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine(Roll_N);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass cc = new ChildClass();
+            cc.GetData(44);
+            cc.ShowData();
+            Console.ReadKey();
+        }
+    }
+}
