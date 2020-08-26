31b1fe930d267d4bfccaec7f65ee17ca2be6ddcc
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:09:28 +0530

    Method Overriding Program

---
+++ PolyMethodOverriding/PolyMethodOverriding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PolyMethodOverriding
+{
+    class Figure
+    {
+        public virtual void Drow()
+        {
+            Console.WriteLine("the codding of figure class drow mwethod");
+        }
+
+    }
+    class Rectangle:Figure
+    {
+        public override void Drow()
+        {
+            Console.WriteLine("the coding of Rectangle class drow method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure re = new Rectangle();
+            re.Drow();
+            Console.ReadKey();
+            
+        }
+    }
+}
