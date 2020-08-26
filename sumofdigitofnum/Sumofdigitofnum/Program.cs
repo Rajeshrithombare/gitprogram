70fca727a43623912de7a08863f9761e3defa67e
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:43:08 +0530

    Sum of digit of Number

---
+++ sumofdigitofnum/Sumofdigitofnum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Sumofdigitofnum
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program ob = new Program();
+            string s;
+
+            Console.WriteLine("Enter number");
+            int n = int.Parse(Console.ReadLine());
+            s = ob.mymethod(n);
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+        public string mymethod(int num)
+        {
+            int i = 1;
+            int k = 1;
+            string str;
+            while(i<num)
+            {
+                if(num%i==0)
+                {
+                    k++;
+                    i++;
+                }
+                if (k == 2)
+                {
+                    str = "prime";
+                }
+                else
+                {
+                    str = "not prime";
+                }
+            }
+            return str;
+            
+
+        }
+       
+    }
+}
