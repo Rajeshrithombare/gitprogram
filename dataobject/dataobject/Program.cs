cad92110f2734ef783baa05f77e841762a1676bf
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:18:28 +0530

    Data Object Program

---
+++ dataobject/dataobject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dataobject
+{
+    public class Employee
+    {
+        public int eno;
+        public string ename;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+
+            Console.WriteLine("Enter Enumber and Employee Name");
+
+            e.eno = int.Parse(Console.ReadLine());
+            e.ename = Console.ReadLine();
+
+            Console.WriteLine("eno=" + e.eno);
+            Console.WriteLine("ename=" + e.ename);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
