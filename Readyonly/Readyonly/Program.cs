7a14ab428b95458335bc39141819072ab7c5375a
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:06:41 +0530

    ReadOnly Program

---
+++ Readyonly/Readyonly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Readyonly
+{
+    class sample
+    {
+        public readonly string cname="Raju";
+
+
+        public sample()
+        {
+          
+            Console.WriteLine("Call " + cname);
+            cname = "abc";
+        }
+       
+   
+    }
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            sample ob = new sample();
+            string str = ob.cname;
+           
+            Console.WriteLine("cname is " + str);
+           
+            Console.ReadKey();
+
+        }
+    }
+}
