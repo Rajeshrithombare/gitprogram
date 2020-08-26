a19321809be81bcfbb39dd0ad298812b5fe9e468
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:30:15 +0530

    Implicit Conversion

---
+++ ConversionImplicit/ConversionImplicit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConversionImplicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intvalue = 123;
+
+            long longvalue = intvalue;
+
+            Console.WriteLine("{0}={1}", intvalue, longvalue);
+
+            Console.ReadKey();
+        }
+    }
+}
