512daa77dacc157eb5d24f93abd199ab66c52565
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:35:04 +0530

    Arithmatic Series

---
+++ ArithmeticSeriz3_1/ArithmeticSeriz3_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AssiRoshArithmeticSeriz3_1
+{
+    class Program
+    {//4
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Arithmatic seriz");
+            int j =int.Parse(Console.ReadLine());
+
+            for(int i=1;i<=j;i++)
+            {
+                int Num = i * i*i;
+
+                Console.WriteLine("the range is" + Num);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
