8ce3a105dddbe2cdecf8935b356caac0231e827d
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:07:26 +0530

    Radius Program

---
+++ Radiuse/Radiuse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Radiuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float len, brt;
+            int area;
+
+            Console.WriteLine("enter  the lenght");
+            len = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter the breadth");
+            brt = Convert.ToSingle(Console.ReadLine());
+
+            //area = Convert.ToInt32(Console.ReadLine());
+            area = Convert.ToInt32(len * brt);
+
+            Console.WriteLine(len);
+            Console.WriteLine(brt);
+
+            Console.WriteLine("Area =", + area);
+
+
+            Console.ReadKey();
+          }
+    }
+}
