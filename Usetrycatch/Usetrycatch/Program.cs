00851f743e3f26e351f6791b39bc671efaa71abc
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:32:03 +0530

    Use Try Catch

---
+++ Usetrycatch/Usetrycatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Usetrycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter number");
+                int i = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number is" + i);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e);
+
+            }
+            catch(DivideByZeroException caught)
+            {
+                Console.WriteLine(caught.Message);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Code reach till the end");
+            }
+
+        }
+    }
+}
