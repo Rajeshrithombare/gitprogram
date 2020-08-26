dfd685f2e0cdda4fae390fdf0decd809f8202e8c
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:28:54 +0530

    Choice for addition or substraction

---
+++ Choiseforaddsub/Choiseforaddsub/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Choiseforaddsub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            int num1, num2, res;
+
+            Console.WriteLine("Ente the 1 for addition");
+            Console.WriteLine("Enter 2 for substaction");
+            ch = Convert.ToInt32(Console.ReadLine());
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            switch(ch)
+            {
+                case 1:
+                    res = num1 + num2;
+                    Console.WriteLine("Result = " + res);
+                    break;
+
+                case 2:
+                    res = num1 - num2;
+                    Console.WriteLine("Result = " + res);
+                    break;
+
+                default:
+                    Console.WriteLine("Not");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
