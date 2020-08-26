334bcf73eeebea78e56f5d84d599088bc4b10de2
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:16:47 +0530

    Program by using this statement

---
+++ usingthisnwpro/usingthisnwpro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace usingThis
+{
+    class Bank
+    {
+        private string name;
+
+        public void sename(string name)
+        {
+            this.name = name;
+        }
+        public void showdata()
+        {
+            Console.WriteLine(name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b = new Bank();
+            b.sename("ravi");
+            b.showdata();
+            Console.ReadKey();
+        }
+    }
+}
