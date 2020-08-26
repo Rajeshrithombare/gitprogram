cbb99c54378bb6689495832b164bbd731c7a3a20
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:08:26 +0530

    Print 10 Number

---
+++ Print10number/Print10number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Print10number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10, i = 1;
+
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            i = Convert.ToInt32(Console.ReadLine());
+
+            while(i<=num)
+            {
+                
+                i++;
+                Console.WriteLine("Number is= " + i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
