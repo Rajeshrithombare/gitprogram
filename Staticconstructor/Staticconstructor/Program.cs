fd06c2c021fc4b433a65449f81f60bdb71c0958e
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:01:31 +0530

    Static constructor Program

---
+++ Staticconstructor/Staticconstructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Staticconstructor
+{
+    public class Adult
+    {
+        private static int minimunage;//member variable with private and static
+        static Adult()
+        {
+            minimunage = 18;
+        }
+        public void showdata()
+        {
+            Console.WriteLine("age=" + minimunage);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adult a = new Adult();
+            a.showdata();
+            Console.ReadKey();
+
+        }
+    }
+}
