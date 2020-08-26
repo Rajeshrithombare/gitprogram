bd160e03635703f154f8731e51c5aeb4584e6871
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:36:04 +0530

    Constructor overloading Program

---
+++ ConstructOverloading/ConstructOverloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConstructOverloading
+{
+    public class Employee
+    {
+        public int salory;
+
+        public Employee(int anualsalory)
+        {
+            salory = anualsalory;
+        }
+        public Employee(int weelysalory,int numberofweek)
+        {
+            salory = weelysalory * numberofweek;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee(5000);
+            Console.WriteLine("annualsalory" + e.salory);
+
+            Employee ee = new Employee(700, 4);
+            Console.WriteLine("monthSalory" + ee.salory);
+
+            Console.ReadKey();
+        }
+    }
+}
