8b9e2874b4f1ddc9b93b08f5014c9a8832775eed
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:10:25 +0530

    Star Pattern Program

---
+++ PaternStar/PaternStar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PaternStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=1;i<4;i++)
+            {
+
+                Console.WriteLine("*");
+            }
+           
+            for (int sp=1;sp>2;sp--)
+            {
+                Console.WriteLine(sp);
+                
+                if(sp==5)
+                {
+                    Console.WriteLine("*");
+                }
+                for (i = 1+1; i < 4; i++)
+                {
+
+                    Console.WriteLine("*");
+                }
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
