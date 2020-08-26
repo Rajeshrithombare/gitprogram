a7f5a33012bb22b6b8cd9bdcc0f3ddd1666e3f1e
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:54:16 +0530

    Swap two number by using therd veriable

---
+++ Swaptonumbyusingthrdveriabl/Swaotonum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Swaotonum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            int temp;
+
+            Console.WriteLine("Enter the first number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+
+                    temp = n1;
+                    n1 = n2;
+                    n2 = temp;
+
+
+            Console.WriteLine("N1= " + n1);
+            Console.WriteLine("N2= " + n2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
