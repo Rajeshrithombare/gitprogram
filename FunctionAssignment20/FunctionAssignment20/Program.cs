1cc4a52e3d72b38547e67bf1afd6beb9e0f8f476
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:19:44 +0530

    Function Program

---
+++ FunctionAssignment20/FunctionAssignment20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionAssignment20
+{
+    class Program
+    {//1
+        static void Main(string[] args)
+        {
+            Method();  
+        
+        }
+        static void Method()
+        {
+
+
+            Console.WriteLine("Welcome friends!");
+            Console.WriteLine("Have a nice day");
+
+            Console.ReadKey();
+        }
+        
+
+     
+    }
+}
