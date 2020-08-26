c3636ca93b1714bff876dcf6a5953d78d2db82af
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:15:09 +0530

    Number is Vovel or not

---
+++ vovelornotwithif/vovelornotwithif/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace vovelornotwithif
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter the Character");
+            ch = Convert.ToChar(Console.ReadLine());
+            if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u')
+            {
+                Console.WriteLine("Character is vovel");
+            }
+            else
+            {
+                Console.WriteLine("Character is not  vovel");
+            }
+            Console.ReadLine();
+        }
+    }
+}
