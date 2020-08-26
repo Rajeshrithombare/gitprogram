f451e5dd62bf7f491a6c6ab29d5e3f1253723d26
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:32:17 +0530

    Conversion log to int

---
+++ Conversionlogtoint/logtoint/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace logtoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intvalue = 123;
+
+            long longvalue = (int)intvalue;
+
+            Console.WriteLine("{0}={1}" +intvalue, longvalue);
+
+            Console.ReadKey();
+        }
+    }
+}
