3a9d60fe8958dd05d75a4ea45160e8af26f8e74c
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:50:04 +0530

    Use Bhasker Formula

---
+++ bhaskersformula/bhaskersformula/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bhaskers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float formula;
+            int a, b, c;
+
+            Console.WriteLine("Enter three number");
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+                formula=b*b-(4*a*c);
+
+            Console.WriteLine(formula);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
