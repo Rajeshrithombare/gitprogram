15b752ec6ddc43ca0b6bea56de18350584f35b6c
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:14:31 +0530

    Maximum Number between three number

---
+++ Maxbetthree/Maxbetthree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AssignRosh25_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            float av;
+            int dis;
+            float fule;
+
+            Console.WriteLine("Enter the Distence user want to go");
+            dis = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the fule you use");
+            fule = Convert.ToInt32(Console.ReadLine());
+
+             float res=dis + fule;
+             av = res / 2;
+
+            Console.WriteLine("Avrage is=" + av);
+
+            Console.ReadKey();
+        }
+
+    }
+}
