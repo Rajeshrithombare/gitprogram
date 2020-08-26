bdaf4322677ff81af064702e58c097e196a5826b
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:41:47 +0530

    Number is divisible by 4 or not

---
+++ numdivby4ornot/numdivby4ornot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numdivby4ornot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the Number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 4==0)
+
+                Console.WriteLine("Number is divisible by 4");
+
+            else
+                Console.WriteLine("Number is not divisible by 4");
+
+            Console.ReadKey();
+
+        }
+    }
+}
