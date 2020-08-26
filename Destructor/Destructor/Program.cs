f162433cf223ca21c4c84a2d8eccfb7922c89ecd
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:41:00 +0530

    Destructor Example

---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Destructor
+{
+    class car
+    {
+        public car()
+        {
+            Console.WriteLine("Constructor called");
+        }
+        ~car()
+        {
+            Console.WriteLine("destructor is called");
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car c = new car();
+
+            Console.ReadKey();
+           
+        }
+    }
+}
