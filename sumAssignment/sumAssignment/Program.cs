7406310dfb99e75d27d689f24535da7a7d2f72a7
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:21:36 +0530

    Sum Program

---
+++ sumAssignment/sumAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int res = 0;
+            int i;
+            Console.WriteLine("Enter 5 array element");
+            for(i=0;i<5;i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+
+            }
+            for(i=0;i<5;i++)
+            {
+                res = a[i] + res;
+
+            }
+            Console.WriteLine("the sum of element is= "+res);
+            Console.ReadKey();
+        }
+    }
+}
