a310351bcff05cd3d10e24bcd8d358f9bb42190b
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:36:56 +0530

    Windows form

---
+++ UsecalclassWindowsFormsApp26/WindowsFormsApp26/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewClassLibrary;
+namespace WindowsFormsApp26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculator c = new Calculator();
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int result = c.Addition(n1, n2);
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Calculator c = new Calculator();
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int result = c.Substraction(n1, n2);
+            textBox3.Text = result.ToString();
+        }
+    }
+}
