26f38dc3122574f25701ede4e7120e369042a8ef
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:48:16 +0530

    Single Inheritance Program

---
+++ singlelinestatement/singlelinestatement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace singlelinestatement
+{
+    public class location
+    {
+        private String locationname;//menber variable
+       
+        public location(string name) => locationname = name;//constructor
+
+        public string name
+        {
+            get => locationname;
+            set => locationname = value;
+        }
+        public void showdata()
+        {
+            Console.WriteLine("name is= " + locationname);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            location l = new location("ravi");
+            Console.WriteLine("Enter Name");
+            l.name=Console.ReadLine();
+            l.showdata();
+            Console.ReadKey();
+        }
+    }
+}
