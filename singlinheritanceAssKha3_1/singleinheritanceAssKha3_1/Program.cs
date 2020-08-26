3e5c04c175df01a03c33d8dd093e2c69898961b6
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:37:55 +0530

    Single line inheritance

---
+++ singlinheritanceAssKha3_1/singleinheritanceAssKha3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AssKha3_1
+{
+    class BaseClass
+    {
+        public void Data()
+        {
+            Console.WriteLine("This is Parent class");
+        }
+    }
+    class ChildClass:BaseClass
+    {
+        public void Data2()
+        {
+            Console.WriteLine("this is child class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new BaseClass();
+            bc.Data();
+
+            ChildClass cc = new ChildClass();
+            cc.Data2();
+
+            cc.Data();
+        }
+    }
+}
