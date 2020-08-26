2f41d188e99f0117a0bf7abb3365d7831a6ae4e7
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:21:40 +0530

    Add Sub

---
+++ Enterno.addsubWindowsForms/enternosubaddWindowsForms/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using MyLibrory;
+
+namespace mynewWindowsFormsApp
+{
+    public partial class Form1 :Form
+    {
+        Calculation c = new Calculation();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int result = c.Addition(n1, n2);
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int result = c.Substraction(n1, n2);
+            textBox3.Text = result.ToString();
+        }
+    }
+}
