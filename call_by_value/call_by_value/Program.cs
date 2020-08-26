90200e9c73f40b1836021b268b26882cf8bb08c2
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:11:43 +0530

    Call By value Program

---
+++ call_by_value/call_by_value/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace call_by_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            Console.WriteLine(sum);
+            //                int a = 2, b = 3;
+            sum =Method();
+           
+            Console.ReadKey();
+
+        }
+        static int Method(   )
+        {
+            int res=5;
+            //res = c + d;
+            return res;
+            
+        }
+    }
+}
