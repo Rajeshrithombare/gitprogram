a1120759cb412c2b1df40b67d066dadeb93f01f5
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:27:47 +0530

    Enter number show in word

---
+++ Enternumadshowinword/Enternumadshowinword/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enternumadshowinword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Four");
+                    break;
+
+                default:
+                    Console.WriteLine("Some other number");
+                    break;
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
