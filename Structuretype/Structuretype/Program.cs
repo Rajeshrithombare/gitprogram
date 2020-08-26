dd4c4a0481d21c9c958cd192ab76d1c20c1b6e99
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:40:42 +0530

    Structure Program

---
+++ Structuretype/Structuretype/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Structuretype
+{
+    struct Employee
+    {
+        public string firstname;
+        public int age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee CompanyEmployee;
+
+            Console.WriteLine("Enter the name and age");
+            CompanyEmployee.firstname = Console.ReadLine();
+
+            CompanyEmployee.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("FirstName= " + CompanyEmployee.firstname);
+            Console.WriteLine("age= " + CompanyEmployee.age);
+
+
+            Console.ReadKey();
+
+       
+        }
+    }
+}
