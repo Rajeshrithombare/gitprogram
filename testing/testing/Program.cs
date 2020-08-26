ea7af58210495a1b8eacc0a765f1199cabb85055
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:14:15 +0530

    Testing Program

---
+++ testing/testing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class BasicMaths
+    {
+        public double Add(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+    
+}
