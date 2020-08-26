951dec4359dd559432b7d6dd1ffd94c9a4db8d48
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:39:28 +0530

    Defind Temprature

---
+++ Temprature/Temprature/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Temprature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float temp, c;
+
+            Console.WriteLine("Enter the temprature");
+            temp = Convert.ToSingle(Console.ReadLine());
+
+                   c = (5.0f / 9.0f) * (temp - 32);
+
+            Console.WriteLine("c=" + c);
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
