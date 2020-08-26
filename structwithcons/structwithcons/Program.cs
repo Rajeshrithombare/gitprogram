79c230408a0622c6e6adb5200420d8c4acb05173
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 13:43:24 +0530

    structure Program with constructor

---
+++ structwithcons/structwithcons/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace structwithcons
+{
+    public struct Employee
+    {
+        string firstname;
+        int age;
+        //Constructur
+        public Employee(string a,int b)
+        {
+            firstname = a;
+            age = b;
+        }
+        //method
+        public void showemmployeedata()
+        {
+            Console.WriteLine("Firstname" + firstname);
+            Console.WriteLine("Age" + age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name and age");
+
+            string a = Console.ReadLine();
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Employee CompanyEmployee = new Employee(a, b);
+            CompanyEmployee.showemmployeedata();
+
+            Console.ReadKey();
+        }
+    }
+}
