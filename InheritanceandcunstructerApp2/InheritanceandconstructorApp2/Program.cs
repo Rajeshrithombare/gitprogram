c17dac9e59a039741988e22ba730f489a6e3de59
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:16:31 +0530

    Inheritance and structure program

---
+++ InheritanceandcunstructerApp2/InheritanceandconstructorApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+
+    class Token
+    {
+        protected Token(string name)
+        {
+            Console.WriteLine("Base class Constructor= " + name);
+        }
+    }
+
+    class CommentToken:Token
+    {
+        public CommentToken(string name):base(name)
+        {
+            Console.WriteLine("derived class " + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommentToken ct = new CommentToken("mk"); 
+
+            Console.ReadKey();
+        }
+    }
+}
