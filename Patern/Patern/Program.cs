22bae33565195658d9d5da89d13d469d110453b6
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:11:12 +0530

    Patern Program

---
+++ Patern/Patern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Patern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, size;
+            size = Convert.ToInt32(Console.ReadLine());
+            for(i=size;i>=1;i--)
+            {
+                for(j=0;j<=i;j++)
+                {
+                    Console.Write(" " + j);
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
