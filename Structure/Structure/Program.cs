26e97ed23e4be155aba27a40ff1e37e17c12a6bc
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:51:29 +0530

    Structure Program

---
+++ Structure/Structure/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Structure
+{
+    public struct Student
+    {
+        public string name;
+        public int age;
+
+        public Student(string a,int b)
+        {
+            name = a;
+            age = b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Student classstudent = new Student();  
+
+             Console.WriteLine("Enter the name of student");
+             string name = Console.ReadLine();
+
+             Console.WriteLine("Enter the age of student");
+             int age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Name = " + classstudent.name);
+                Console.WriteLine("Age = " + classstudent.age);
+
+
+                Console.ReadKey();
+
+           
+        }
+    }
+}
