1175e587cd09b53009249890559404fbe65bafef
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:31:11 +0530

    Eligible for vote or not

---
+++ Eligiblityforvote/Eligiblity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Eligiblity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            
+            Console.WriteLine("Enter the age");
+
+             age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Age " + age);
+
+            if (age>= 18)
+            {
+                Console.WriteLine("Elligible for vote");
+            }
+            else
+            {
+                Console.WriteLine("Not Elligible for vote");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
