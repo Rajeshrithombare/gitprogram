0fdce01fb903c8585d309bcaff9088d688095679
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:04:47 +0530

    Seal Class Program

---
+++ Sealclass/SealClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SealClass
+{
+    interface IToken
+    {
+        string Name();
+    }
+   interface IVissibal
+    {
+       void Name();
+    }
+
+    class Token:IToken,IVissibal
+    {
+        string IToken.Name()
+        {
+            return "hello from Name";
+        }
+        void IVissibal.Name()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IToken t = new Token();
+            Console.WriteLine(t.Name());
+            IVissibal t1 = new Token();
+            t1.Name();
+
+            Console.ReadKey();
+        }
+    }
+}
