a3e87d2694387ac7c5048799b11caf67a104b825
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:19:51 +0530

    Class Library Program

---
+++ addsubClassLibrary/NewClassLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewClassLibrary
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            int res = a + b;
+            return res;
+        }
+        public int Substraction(int a, int b)
+        {
+            int res = a - b;
+            return res;
+        }
+    }
+}
