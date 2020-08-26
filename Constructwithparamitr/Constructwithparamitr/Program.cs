7f0e3190d0c0242dd46b09426921312671766207
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:37:12 +0530

    Parameteris Constructor Program

---
+++ Constructwithparamitr/Constructwithparamitr/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//constructor with paramiter
+namespace Constructwithparamitr
+{
+    public class person //class
+    {
+        private string last;//member variabl
+        private string first;
+        
+
+        public person(string lastname,string firstname)//constructor
+        {
+            last = lastname;
+            first = firstname;
+        }
+        public void showdata()//method
+        {
+            Console.WriteLine("first= " + first);
+            Console.WriteLine("last= " + last);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person ob = new person("kumar","rajesh");
+            ob.showdata();
+
+            Console.ReadKey();
+
+        }
+    }
+}
