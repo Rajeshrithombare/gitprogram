e07382046daf935d8493dfe000a5b602cdd7f3df
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 15:29:47 +0530

    Character is vovel or not

---
+++ Vovelornotwithswitch/Vovelornotwithswitch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vovelornotwithswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter the Character");
+            ch = Convert.ToChar(Console.ReadLine());
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("Character is vovel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Character is vovel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Character is vovel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Character is vovel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Character is vovel");
+                    break;
+                default:
+                    Console.WriteLine("Character is not vovel");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
