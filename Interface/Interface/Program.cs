7ac7bc2933cae1e6026e8e5dc36384e21159323f
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:15:17 +0530

    Interface Program

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Interface
+{
+    interface Account
+    {
+        public void Deposite(int amt);
+    }
+    interface Account1:Account
+    {
+       void ShowBalence();
+    }
+    class Saving:Account1
+    {
+        public void Deposite(int amt)
+        {
+            Console.WriteLine("Deposite");
+        }
+        public void ShowBalence()
+        {
+            Console.WriteLine("show");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Account1 act = new Saving();
+            act.Deposite(2);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
