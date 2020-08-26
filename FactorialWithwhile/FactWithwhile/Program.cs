574366ef8552b926e17eaf4cd6ffb37d2d73b7c2
Rajeshrithombare <rajeshrithombare944@gmail.com>
2020-08-26 16:25:52 +0530

    Factorial Program by using while

---
+++ FactorialWithwhile/FactWithwhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FactWithwhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 1, num;
+
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while(num>0)
+            {
+                f = num * f;
+                num--;
+               
+            }
+            Console.WriteLine("Factorial is=" + f);
+
+            Console.ReadKey();
+        }
+    }
+}
